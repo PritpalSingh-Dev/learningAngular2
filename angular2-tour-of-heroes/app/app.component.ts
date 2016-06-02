2c2ff647f5c8875662aedd088c24f68f81e6121f
psbadewal <psbadewal@icloud.com>
2016-06-02 20:22:34 +0100

    Setting up app.component and main.ts files

--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -1,4 +1,8 @@
-export class AppComponent {
-  title = "Tour of Heroes";
-  hero = "Windstorm";
-}
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>My first Angular 2 App</h1>'
+})
+
+export class AppComponent { }
